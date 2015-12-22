d146f9b1d02f5a095a4fd37a54fd0384f8e5eea4
Ronald So <ronald.so@icloud.com>
2015-12-22 16:05:57 -0500

    README + first stub

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# coach
-Chat app
+This is my personal playground to test out Meteor Angular with the Whatsapp
+example.  It may contain tests that result non-working code.
---
+++ client/scripts/controllers/chats.controller.js
@@ -0,0 +1,21 @@
+angular
+  .module('Coach')
+  .controller('ChatsCtrl', ChatsCtrl);
+
+function ChatsCtrl ($scope, $reactive) {
+  $reactive(this).attach($scope);
+
+  this.remove = remove;
+
+  this.helpers({
+    data() {
+      return Chats.find();
+    }
+  });
+
+  ////////////
+
+  function remove (chat) {
+    this.data.remove(chat);
+  }
+}
